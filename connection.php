60bac81c93a35bf9484ba60e3d5049c51e9aa8eb
D Murphy <rexmicrosoft@yahoo.com.ph>
2019-05-14 22:34:15 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php 
 session_start();
 date_default_timezone_set('Asia/Manila');
-$connection = mysqli_connect("localhost", "root", "vertrigo", "tam_db");
+$connection = mysqli_connect("localhost", "root", "", "tam_db");
 ?>
